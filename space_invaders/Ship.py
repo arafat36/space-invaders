08369bb4a0b17855c918f752acadbffa16b0291a
Mazkdoktor <49337883+Mazkdoktor@users.noreply.github.com>
2020-11-04 19:40:46 +0100

    Update Ship.py
    Added Ship class

--- space_invaders/Ship.py
+++ space_invaders/Ship.py
@@ -1,4 +1,29 @@
+from pygame.rect import Rect
+from pygame.surface import Surface
+from pygame.mask import Mask
+from pygame.sprite import GroupSingle, Sprite
 
-class Ship():
-    """ """
-    pass
+class Ship(GroupSingle):
+    def __init__(self):
+        super(Ship, self).__init__()
+        self.image = Surface((100, 50))
+        self.rect = self.image.get_rect()
+        self.mask = Mask((self.image.get_width(), self.image.get_height()))
+        self.sprite = Sprite(self)
+
+        self.move_speed = 10
+
+    def get_center():
+        return self.rect.center
+
+    def get_left():
+        return self.rect.left
+
+    def get_right():
+        return self.rect.right
+
+    def move_left():
+        self.rect.move_ip(-self.move_speed, 0)
+
+    def move_right():
+        self.rect.move_ip(self.move_speed, 0)
